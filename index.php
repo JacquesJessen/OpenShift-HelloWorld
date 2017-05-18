4e2d5f6bc6073b6dc84141d69804992d5b1823f7
JacquesJessen <jacques.jessen@gmail.com>
2017-05-18 15:02:55 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
-/*
+<!--
 https://www.youtube.com/watch?v=RuvpraMqhGI
+
 https://github.com/sjenning/origin-php-helloworld/blob/master/index.php
 Seth Jennings sjenning
 Kubernetes/OpenShift developer at Red Hat
-*/
+-->
 
 <?php
 echo "hello world v1.0 from server: ".gethostname()
